c61b570fb8e2e9a6b16c426ab14a0fda0c0595be
Victor Deleau <deleau.victor.pro@gmail.com>
2023-10-09 21:18:03 +0200

    fix broken link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
 				<div className={styles.buttons}>
 					<Link
 						className='button button--secondary button--lg'
-						to='/docs/Langages/CSharp/1'
+						to='/docs/csharp/csharp/index.mdx'
 					>
 						Présentation de C#
 					</Link>
